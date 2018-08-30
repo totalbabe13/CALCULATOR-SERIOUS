c9f5a48dafda6c9395a5fa645a8fd068d7b683a4
leonardo Kaplan <kaplanleonardo@gmail.com>
2018-08-30 12:16:34 -0500

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+// alert('YOLO'); linked!
+
+// ARITHMETIC FUNCTIONS:
+
+function addingNumbers (x,y) {
+	 console.log(x + y);	
+}
+
+function subtract (x,y) {
+	 console.log(x - y);
+}
+
+
+
+
+//BUTTON elements positions:
+
+let viewer = document.getElementById('numberViewer');
+
+// GOAL : get the calulator to display the number 0
+let positionZero = document.getElementById('zeroButton');
+
+
+// NUMBER FUNCTIONS:
+
+function displayZero() {
+	
+	viewer.textContent += "0";
+
+
+}
