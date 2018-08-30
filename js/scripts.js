b30a0145661bd42e910b157b1d02b1a69035b502
leonardo Kaplan <kaplanleonardo@gmail.com>
2018-08-30 12:46:24 -0500

    all number buttons display on viewer when clicked

--- js/scripts.js
+++ js/scripts.js
@@ -20,12 +20,64 @@ let viewer = document.getElementById('numberViewer');
 // GOAL : get the calulator to display the number 0
 let positionZero = document.getElementById('zeroButton');
 
+let positionOne = document.getElementById('oneButton'); //HTML not updated yet
+let positionTwo = document.getElementById('twoButton'); //HTML not updated yet
+let positionThree = document.getElementById('threeButton'); //HTML not updated yet
+let positionFour = document.getElementById('fourButton'); //HTML not updated yet
+let positionFive = document.getElementById('fiveButton'); //HTML not updated yet
+let positionSix = document.getElementById('sixButton'); //HTML not updated yet
+let positionSeven = document.getElementById('sevenButton'); //HTML not updated yet
+let positionEight = document.getElementById('eightButton'); //HTML not updated yet
+let positionNine = document.getElementById('NineButton'); //HTML not updated yet
+
 
 // NUMBER FUNCTIONS:
 
 function displayZero() {
-	
 	viewer.textContent += "0";
-
-
 }
+
+function displayOne() {
+	viewer.textContent += "1";
+}
+
+function displayTwo() {
+	viewer.textContent += "2";
+}
+
+function displayThree() {
+	viewer.textContent += "3";
+}
+
+function displayFour() {
+	viewer.textContent += "4";
+}
+
+function displayFive() {
+	viewer.textContent += "5";
+}
+
+function displaySix() {
+	viewer.textContent += "6";
+}
+
+function displaySeven() {
+	viewer.textContent += "7";
+}
+
+function displayEight() {
+	viewer.textContent += "8";
+}
+
+function displayNine() {
+	viewer.textContent += "9";
+}
+
+
+
+
+
+
+
+
+
