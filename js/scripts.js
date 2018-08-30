6fb657a0f0d98166d0dfeea696af69761cd0fe85
leonardo Kaplan <kaplanleonardo@gmail.com>
2018-08-30 16:40:15 -0500

    configuring how to add multiple values in order given

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,40 @@
-// alert('YOLO'); linked!
+
 
 // ARITHMETIC FUNCTIONS:
 
-function addingNumbers (x,y) {
-	 console.log(x + y);	
+
+let oppArray = [];
+
+function additionDisplay() {
+	 viewer.textContent = "+"
+     
+     if(oppArray.length > 3) {
+     	let operationString = oppArray.join('');
+	    let results = eval(operationString);
+	    oppArray =[results];
+     };
+
+     if( oppArray[oppArray.length-1] !== "+" && oppArray[oppArray.length-1] !== "-" && oppArray[oppArray.length-1] !== "*" && oppArray[oppArray.length-1] !== "/" ){
+         oppArray.push("+")
+    }; 
+    console.log(oppArray)
 }
 
-function subtract (x,y) {
-	 console.log(x - y);
+
+function equalsResults(){
+	 let operationString = oppArray.join('');
+	 let results = eval(operationString);
+	// console.log(operationString)
+	 console.log(results);
+	 viewer.textContent = `${results}`;
 }
-
-
-
 
 //BUTTON elements positions:
 
 let viewer = document.getElementById('numberViewer');
 
-// GOAL : get the calulator to display the number 0
-let positionZero = document.getElementById('zeroButton');
 
-let positionOne = document.getElementById('oneButton'); //HTML not updated yet
-let positionTwo = document.getElementById('twoButton'); //HTML not updated yet
-let positionThree = document.getElementById('threeButton'); //HTML not updated yet
-let positionFour = document.getElementById('fourButton'); //HTML not updated yet
-let positionFive = document.getElementById('fiveButton'); //HTML not updated yet
-let positionSix = document.getElementById('sixButton'); //HTML not updated yet
-let positionSeven = document.getElementById('sevenButton'); //HTML not updated yet
-let positionEight = document.getElementById('eightButton'); //HTML not updated yet
-let positionNine = document.getElementById('NineButton'); //HTML not updated yet
-
-
-// NUMBER FUNCTIONS:
+// NUMBER DISPLAY FUNCTIONS:
 
 function displayZero() {
 	viewer.textContent += "0";
@@ -39,14 +42,43 @@ function displayZero() {
 
 function displayOne() {
 	viewer.textContent += "1";
-}
+	oppArray.push("1");
+	console.log (oppArray);
+
+
+ //   if(checkOperation(currentOperator) === true){
+ //   	valueOne += "1" ;
+	// viewer.textContent += "1";
+	// console.log(valueOne)
+ // } else {
+	// valueTwo += "1";
+	// viewer.textContent += " 1";
+	// console.log(valueTwo)
+ //  }
+};
 
 function displayTwo() {
-	viewer.textContent += "2";
+	// if(checkOperation(currentOperator) === true){
+ //   	valueOne += "2" ;
+	// viewer.textContent += "2";
+	// console.log(valueOne)
+ // } else {
+	// valueTwo += "2";
+	// viewer.textContent += " 2";
+	// console.log(valueTwo)
+ //  }
 }
 
 function displayThree() {
-	viewer.textContent += "3";
+	// if(checkOperation(currentOperator) === true){
+ //   	valueOne += "3" ;
+	// viewer.textContent += "3";
+	// console.log(valueOne)
+ // } else {
+	// valueTwo += "3";
+	// viewer.textContent += " 3";
+	// console.log(valueTwo)
+ //  }
 }
 
 function displayFour() {
@@ -73,11 +105,45 @@ function displayNine() {
 	viewer.textContent += "9";
 }
 
+function displayDecimal() {
+	viewer.textContent += "."
+}
+
+// OTHER BUTTONS:
+
+function clearDisplay() {
+	viewer.textContent = "";
+}
 
 
 
+function multiplyDisplay() {
+	viewer.textContent = "X"
+}
 
 
+
+	// currentOperator = "+"
+	// console.log(currentOperator)
+	
+// }
+
+
+
+// NUMBER BUTTONS:
+// let positionZero = document.getElementById('zeroButton');
+// let positionOne = document.getElementById('oneButton'); 
+// let positionTwo = document.getElementById('twoButton'); 
+// let positionThree = document.getElementById('threeButton'); 
+// let positionFour = document.getElementById('fourButton'); 
+// let positionFive = document.getElementById('fiveButton'); 
+// let positionSix = document.getElementById('sixButton'); 
+// let positionSeven = document.getElementById('sevenButton'); 
+// let positionEight = document.getElementById('eightButton'); 
+// let positionNine = document.getElementById('NineButton'); 
+
+//OTHER BUTTONS:
+// let positionsClear = document.getElementById('clearButton');
 
 
 
